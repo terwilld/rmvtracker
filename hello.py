3f8541bc76c9364a775b6e8533d111b15465cb2f
david <terwilld@gmail.com>
2015-02-23 21:52:05 -0500

    #1

--- hello.py
+++ hello.py
@@ -1,11 +1,26 @@
-
 import os
-from flask import Flask
+from flask import Flask, render_template, request, url_for
+
+#need forms likbrary
+
+
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+@app.route("/base")
+def base():
+    return render_template('base.html')
+#	return render_template('hello.html')
 
 
+@app.route("/home")
+@app.route("/index")
+@app.route("/")
+def home():
+#	return "hello all"
+	return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
