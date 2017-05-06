256b1385c29d18ed7b8eb00cd65bf04b40ec5d41
David Terwilliger <terwilld@gmail.com>
2017-05-06 08:34:49 -0400

    shit

--- hello.py
+++ hello.py
@@ -80,7 +80,7 @@ def test2():
         print 'made connection'
       #cur is the cursor which is used to execute all PSQL queries
         print type(url.path[1:])
-        print 'database type : ' + type(url.path[1:]) + 'Database name: ' + str(url.path[1:])
+        print 'database type : ', type(url.path[1:]), 'Database name: ', str(url.path[1:])
         print 'user type : ' + type(url.username) + 'user:  ' + str(url.username)
         print 'password type: ' + type(url.password) + 'password: ' + str(url.password)
         print 'host type : ' + type (url.hostname) + 'host: ' + str(url.hostname)
