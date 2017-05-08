80d833a1286ef709f65377985851a26a975b20e1
David Terwilliger <terwilld@gmail.com>
2017-05-08 19:41:21 -0400

    adjusted the sceduler

--- clock.py
+++ clock.py
@@ -2,12 +2,14 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+
+# @sched.scheduled_job('interval', minutes=3)
+# def timed_job():
+#     print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=6-22,minute=*/3)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    print('This job is run every weekday every 3 minutes.')
 
 sched.start()
