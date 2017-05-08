96ee4384395c8785b4311400072cf558a0359e7d
David Terwilliger <terwilld@gmail.com>
2017-05-08 19:54:35 -0400

    scheduler problems

--- clock.py
+++ clock.py
@@ -7,9 +7,12 @@ sched = BlockingScheduler()
 # @sched.scheduled_job('interval', minutes=3)
 # def timed_job():
 #     print('This job is run every three minutes.')
-
+print 'test_1_!_1'
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour='6-22',minute='*/3')
 def scheduled_job():
     print('This job is run every weekday every 3 minutes.')
+
+
+print 'test_2_@_2_2_@'
 
 sched.start()
