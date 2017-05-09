7a724d882c7f71344618a2b61b72272bec5874a4
David Terwilliger <terwilld@gmail.com>
2017-05-08 21:22:22 -0400

    changed order of APS

--- clock.py
+++ clock.py
@@ -7,6 +7,12 @@ sched = BlockingScheduler()
 # @sched.scheduled_job('interval', minutes=3)
 # def timed_job():
 #     print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour='6-22', minute='*/3')
+def scheduled_job_1():
+    print('This job is run every weekday every 3 minutes with the hour addition.')
+
+
 print 'test_1_!_1'
 @sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/3')
 def scheduled_job():
@@ -14,9 +20,6 @@ def scheduled_job():
     print '123432123'
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour='6-22', minute='*/3')
-def scheduled_job_1():
-    print('This job is run every weekday every 3 minutes with the hour addition.')
 
 print 'test_2_@_2_2_@'
 
