ff9f9e8a1c392c4a778ac1bb6b25043e3b1b82c5
David Terwilliger <terwilld@gmail.com>
2017-05-10 17:25:10 -0400

    added time range

--- clock.py
+++ clock.py
@@ -1,22 +1,12 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-
 from scrape_locations import *
 import psycopg2,urlparse
 from subprocess import Popen, PIPE
 
-
 sched = BlockingScheduler()
-
-
-
-
-#nine_hours_from_now = datetime.now() + timedelta(hours=9)
-#date_time=datetime.now().isoformat()
-#print 'Date time without timezone specified: ' + date_time 
 
 	#Gather DB credentials 
 try:
-
     print 'clocks.py: no config imported: this is a deployed build'
     urlparse.uses_netloc.append("postgres")
     url = urlparse.urlparse(os.environ["DATABASE_URL"])
@@ -34,17 +24,10 @@ try:
   
 except:
 	print 'clocks data base credentials failed to gather'
-
-
-	# Make current data table if it doesn't exist
 try:
-    print 'got in'
-    conn = psycopg2.connect(database=DB_name,user=DB_user,password=DB_password,host=DB_host,port=DB_port,sslmode='require')
-    print 'made connection'
-    cur = conn.cursor()
-    
-    print 'test inside try make table'
 
+    conn = psycopg2.connect(database=DB_name,user=DB_user,password=DB_password,host=DB_host,port=DB_port,sslmode='require')
+    cur = conn.cursor()
     cur.execute("CREATE TABLE Current_Data(Date_time VARCHAR(35),\
 	    Attleboro_Licensing INT, Attleboro_Registration INT, \
 	    Boston_Licensing INT, Boston_Registration INT, \
@@ -88,81 +71,64 @@ except:
 
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour='6-22', minute='*/3')
-def scheduled_job_1():
-    print('This job is run every weekday every 3 minutes with the hour addition.')
-    tz = timezone('America/New_York')
-    date_time=datetime.now(tz).isoformat()
-    print 'Date time: ' + date_time    
-
-
-
-
-# #Switch to this remove lots of shit
-# timestamp = datetime.datetime.now(pytz.timezone("America/New_York")).time()
-# start,end = datetime.time(7), datetime.time(19)
-
-# if start <= timestamp <= end:
-#     print 'do this its true'
-
-
-
-
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour='6-22', minute='*/3')
+# def scheduled_job_1():
+#     print('This job is run every weekday every 3 minutes with the hour addition.')
+#     tz = timezone('America/New_York')
+#     date_time=datetime.now(tz).isoformat()
+#     print 'Date time: ' + date_time    
 
 
 print 'test_1_!_1'
 @sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/10')
 def scheduled_job():
-    print('This job is run every weekday every 3 minutes without the hour addition.')
-    list_of_towns=['Attleboro','Boston','Braintree','Brockton','Chicopee','Easthampton','Fall%20River','Greenfield','Haverhill','Lawrence','Leominster','Lowell','Martha%27s%20Vineyard','Milford','Nantucket','Natick','New%20Bedford','North%20Adams','Pittsfield','Plymouth','Revere','Roslindale','South%20Yarmouth','Springfield','Taunton','Watertown','Wilmington','Worcester']
-    Result_List=add_a_reading(list_of_towns)
-
-
-
-    print Result_List
-
-    conn = psycopg2.connect(database=DB_name,user=DB_user,password=DB_password,host=DB_host,port=DB_port,sslmode='require')
-
-    cur = conn.cursor()
-    print 'about to try and place in the database'
-
-    cur.execute("INSERT into Current_data(Date_time, \
-        Attleboro_Licensing, Attleboro_Registration, \
-        Boston_Licensing, Boston_Registration, \
-        Braintree_Licensing, Braintree_Registration, \
-        Brockton_Licensing, Brockton_Registration, \
-        Chicopee_Licensing, Chicopee_Registration, \
-        Easthampton_Licensing, Easthampton_Registration, \
-        Fall_River_Licensing, Fall_River_Registration, \
-        Greenfield_Licensing, Greenfield_Registration, \
-        Haverhill_Licensing, Haverhill_Registration, \
-        Lawrence_Licensing, Lawrence_Registration, \
-        Leominster_Licensing, Leominster_Registration, \
-        Lowell_Licensing, Lowell_Registration, \
-        Marthas_Vineyard_Licensing, Marthas_Vineyard_Registration, \
-        Milford_Licensing, Milford_Registration, \
-        Nantucket_Licensing, Nantucket_Registration, \
-        Natick_Licensing, Natick_Registration, \
-        New_Bedford_Licensing, New_Bedford_Registration, \
-        North_Adams_Licensing, North_Adams_Registration, \
-        Pittsfield_Licensing, Pittsfield_Registration, \
-        Plymouth_Licensing, Plymouth_Registration, \
-        Revere_Licensing, Revere_Registration, \
-        Roslindale_Licensing, Roslindale_Registration, \
-        South_Yarmouth_Licensing, South_Yarmouth_Registration, \
-        Springfield_Licensing, Springfield_Registration, \
-        Taunton_Licensing, Taunton_Registration, \
-        Watertown_Licensing, Watertown_Registration, \
-        Wilmington_Licensing, Wilmington_Registration, \
-        Worcester_Licensing, Worcester_Registration) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s)", Result_List)
-    print'may have actually put into the database'
-    conn.commit()
-    conn.close()
-
-
+    print('This job is run every weekday every 10 minutes without the hour addition.')
+    timestamp,start,end = datetime.datetime.now(pytz.timezone("America/New_York")).time(),datetime.time(7), datetime.time(19)
+    if start <= timestamp <= end:
+        try:
+            print 'inside time range, execute'
+            list_of_towns=['Attleboro','Boston','Braintree','Brockton','Chicopee','Easthampton','Fall%20River','Greenfield','Haverhill','Lawrence','Leominster','Lowell','Martha%27s%20Vineyard','Milford','Nantucket','Natick','New%20Bedford','North%20Adams','Pittsfield','Plymouth','Revere','Roslindale','South%20Yarmouth','Springfield','Taunton','Watertown','Wilmington','Worcester']
+            Result_List=add_a_reading(list_of_towns)
+            print Result_List
+            conn = psycopg2.connect(database=DB_name,user=DB_user,password=DB_password,host=DB_host,port=DB_port,sslmode='require')
+            cur = conn.cursor()
+            print 'about to try and place in the database'
+            cur.execute("INSERT into Current_data(Date_time, \
+                Attleboro_Licensing, Attleboro_Registration, \
+                Boston_Licensing, Boston_Registration, \
+                Braintree_Licensing, Braintree_Registration, \
+                Brockton_Licensing, Brockton_Registration, \
+                Chicopee_Licensing, Chicopee_Registration, \
+                Easthampton_Licensing, Easthampton_Registration, \
+                Fall_River_Licensing, Fall_River_Registration, \
+                Greenfield_Licensing, Greenfield_Registration, \
+                Haverhill_Licensing, Haverhill_Registration, \
+                Lawrence_Licensing, Lawrence_Registration, \
+                Leominster_Licensing, Leominster_Registration, \
+                Lowell_Licensing, Lowell_Registration, \
+                Marthas_Vineyard_Licensing, Marthas_Vineyard_Registration, \
+                Milford_Licensing, Milford_Registration, \
+                Nantucket_Licensing, Nantucket_Registration, \
+                Natick_Licensing, Natick_Registration, \
+                New_Bedford_Licensing, New_Bedford_Registration, \
+                North_Adams_Licensing, North_Adams_Registration, \
+                Pittsfield_Licensing, Pittsfield_Registration, \
+                Plymouth_Licensing, Plymouth_Registration, \
+                Revere_Licensing, Revere_Registration, \
+                Roslindale_Licensing, Roslindale_Registration, \
+                South_Yarmouth_Licensing, South_Yarmouth_Registration, \
+                Springfield_Licensing, Springfield_Registration, \
+                Taunton_Licensing, Taunton_Registration, \
+                Watertown_Licensing, Watertown_Registration, \
+                Wilmington_Licensing, Wilmington_Registration, \
+                Worcester_Licensing, Worcester_Registration) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s)", Result_List)
+            print'may have actually put into the database'
+            conn.commit()
+            conn.close()
+        except:
+            print 'something failed'
 
 print 'test_2_@_2_2_@'
 
+
 sched.start()
-
-
